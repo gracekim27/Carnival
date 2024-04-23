78dafd6641a02d41e2b2374bea9895e83cf92c12
gracekim27 <gracejkim9@gmail.com>
2024-04-23 12:06:17 -0400

    some anims

---
+++ Assets/Scripts/PlayerWalk.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Animator animator;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.MovePosition(transform.position + movement * speed * Time.deltaTime);
+
+        // Update the animator parameters
+        if (movement != Vector3.zero)
+        {
+            animator.SetFloat("Speed", 1.0f);  // Change 1.0 to vary animation speed
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0.0f);
+        }
+    }
+}
+
+
